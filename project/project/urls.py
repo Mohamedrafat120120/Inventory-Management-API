5c6d3dc3f7d40ef6ce4aa03670a0e6f73026d6cf
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-21 00:59:42 +0200

    last commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('inventory_api.urls')),
+    path('api-token/',views.obtain_auth_token)
 ]
