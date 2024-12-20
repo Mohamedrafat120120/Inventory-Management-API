bae82a6cb0438dc49d6bcab783808fc7f24591ec
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-12-20 21:44:48 +0200

    jdsfjlsa

--- project/inventory_api/urls.py
+++ project/inventory_api/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('view_inventorys/',view_inventorys.as_view(),name="view_inventorys"),
     path('view_items/',view_items.as_view(),name="view_items"),
     path('view_speciefic_item/<int:id>/',view_speciefic_item.as_view(),name="view_speciefic_item"),
+    path('add_items/',add_items.as_view(),name="add_items"),
+    path('add_inventorys/',add_inventorys.as_view(),name="add_inventorys"),
+    path('delete_items/',delete_items.as_view(),name="delete_items"),
+    path('delete_inventorys/',delete_inventorys.as_view(),name="delete_inventorys"),
+    path('delete_speciefic_item/',delete_speciefic_item.as_view(),name="delete_speciefic_item"),
+    path('delete_speciefic_invetory/',delete_speciefic_invetory.as_view(),name="delete_speciefic_invetory"),
 
 ]
--- project/inventory_api/views.py
+++ project/inventory_api/views.py
@@ -8,8 +8,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 
 class view_inventorys(APIView):
-    # permission_classes=[IsAuthenticated]
-    # authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
     def get(self,request):
         inventorys=inventory.objects.all()
         serializer=inventoryserialization(inventorys,many=True)
@@ -19,8 +19,8 @@ class view_inventorys(APIView):
 
 
 class view_items(APIView):
-    # permission_classes=[IsAuthenticated]
-    # authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
     def get(self,request):
         items=item.objects.all()
         serializer=itemserialization(items,many=True)
@@ -30,9 +30,70 @@ class view_items(APIView):
         
         
 class view_speciefic_item(APIView):
-    # permission_classes=[IsAuthenticated]
-    # authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
     def get(self,request,id):
         speciefic_item=get_object_or_404(item,pk=id)
         serializer=itemserialization(speciefic_item)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+class add_items(APIView):
+    # permission_classes=[IsAuthenticated]
+    # authentication_classes=[TokenAuthentication]
+    def post(self,request):
+        serializer=itemserialization(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class add_inventorys(APIView):
+    # permission_classes=[IsAuthenticated]
+    # authentication_classes=[TokenAuthentication]
+    def post(self,request):
+        
+        serializer=inventoryserialization(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class delete_items(APIView):
+    def get(self,request):
+       items=item.objects.all()
+       if items:
+           items.delete()
+           return Response({"message":"items deleted"},status=status.HTTP_200_OK)
+       return Response({"message":"no items to delete"},status=status.HTTP_400_BAD_REQUEST)
+   
+   
+class delete_inventorys(APIView):
+    def get(self,request):
+       inventorys=inventory.objects.all()
+       if inventorys:
+           inventorys.delete()
+           return Response({"message":"inventorys deleted"},status=status.HTTP_200_OK)
+       return Response({"message":"no inventorys to delete"},status=status.HTTP_400_BAD_REQUEST)
+   
+class delete_speciefic_item(APIView):
+    def get(self,request,id):
+       item=get_object_or_404(item,pk=id)
+       if item:
+           item.delete()
+           return Response({"message":"item deleted"},status=status.HTTP_200_OK)
+       return Response({"message":"no items to delete"},status=status.HTTP_400_BAD_REQUEST)
+   
+class delete_speciefic_invetory(APIView):
+    def get(self,request,id):
+       inventory=get_object_or_404(inventory,pk=id)
+       if inventory:
+           inventory.delete()
+           return Response({"message":"inventory deleted"},status=status.HTTP_200_OK)
+       return Response({"message":"no inventorys to delete"},status=status.HTTP_400_BAD_REQUEST)
+   
+   
+   
+       
+           
